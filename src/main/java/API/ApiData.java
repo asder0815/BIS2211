de9327ab2a57590e88d1de4eac73d34f471e1c09
asder0815 <56360114+asder0815@users.noreply.github.com>
2020-05-02 15:25:17 +0200

    removed unneccessary comment

--- src/main/java/API/ApiData.java
+++ src/main/java/API/ApiData.java
@@ -38,12 +38,11 @@ public class ApiData
     //returns a https request string with the specified latitude, longitude and radius
     public static String buildRequestString(String lat, String lon, double radius) 
     {
-        //https://creativecommons.tankerkoenig.de/json/list.php?lat=52.521&lng=13.438&rad=1.5&sort=dist&type=all&apikey=00000000-0000-0000-0000-000000000002
         return "https://creativecommons.tankerkoenig.de/json/list.php?lat=" + lat + "&lng=" + lon + "&rad=" + radius + "&sort=dist&type=all&apikey=" + Api_Key.API_KEY;
     }
 
     public static String printStationNames(JsonObject obj) 
-    {
+    {    	
         return obj.toString();
     }
 }
