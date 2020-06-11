bcc04b16160f64a5c79696278362c03c3915fd28
Jonathan Hinkel <asder0815@gmail.com>
2020-06-11 12:24:33 +0200

    added leaderboard and redone test data

--- src/main/java/teamB/BIS2211/TankApp/WebController/LeaderboardController.java
+++ src/main/java/teamB/BIS2211/TankApp/WebController/LeaderboardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import teamB.BIS2211.TankApp.Model.LeaderboardData;
 import teamB.BIS2211.TankApp.Model.LeaderboardDataService;
+import teamB.BIS2211.TankApp.Model.LeaderboardEntry;
 
 @Controller
 public class LeaderboardController 
@@ -22,8 +23,25 @@ public class LeaderboardController
     public String leaderboard(@CookieValue(value = "username", required=false, defaultValue = "") String username, final Model model)
     {
         model.addAttribute("username", username);
-        ArrayList<LeaderboardData> lbData = leaderboardDataService.getAllData(); 
-        model.addAttribute("lbData", lbData);
+        ArrayList<LeaderboardEntry> lbEntries = new ArrayList<LeaderboardEntry>(); 
+        for(String name : getNameList(leaderboardDataService.getAllData()))
+        { 
+            lbEntries.add(new LeaderboardEntry(name, leaderboardDataService.getDataByUser(name))); 
+        }
+        model.addAttribute("leaderboardEntries", lbEntries); 
         return "leaderboard";
+    }
+
+    private static ArrayList<String> getNameList(ArrayList<LeaderboardData> lbData)
+    {
+        ArrayList<String> nameList = new ArrayList<String>();
+        for(LeaderboardData entry : lbData)
+        {
+            if(!nameList.contains(entry.getUser()))
+            {
+                nameList.add(entry.getUser()); 
+            }
+        }
+        return nameList; 
     }
 }
