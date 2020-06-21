86e3398f4736a5a238b66dcfb342c1d2951ce188
Jonathan Hinkel <asder0815@gmail.com>
2020-06-21 21:24:14 +0200

    cleaned up release version

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ im Projektordner ausführen, und das Programm wird gestartet.
 
 Zu beachten:
 
-Bei der ersten Benutzung kann das Starten des Programms eine Wile dauern, da er erst noch einige benötigte Daten herunterladen muss.
+Bei der ersten Benutzung kann das Starten des Programms eine Weile dauern (bis zu 10 Minuten, abhängig von Computer Geschwindigkeit und Betriebssystem), da er erst noch einige benötigte Daten herunterladen muss (dazu wird das folgendes Git-Repo geklont und auch einmal am Tag geupdated: https://github.com/volzinnovation/fuel_price_variations_germany).
--- src/main/java/teamB/BIS2211/TankApp/Model/ScheduledTasks.java
+++ src/main/java/teamB/BIS2211/TankApp/Model/ScheduledTasks.java
@@ -31,14 +31,9 @@ public class ScheduledTasks
     @Autowired
     LeaderboardDataService leaderboardDataService;
 
-    //@Scheduled(fixedRate = 86400000)
-    @Scheduled(fixedRate = 30000)
-    //@Scheduled(cron = "0 30 3 1/1 * ?")
+    @Scheduled(cron = "0 30 3 1/1 * ?")
 	public void databaseMaintenance() {
         System.out.println("Starting database maintenance");
-        //System.out.println("Getting new price data...");
-        //updatePriceDataRepo();
-
         ArrayList<LeaderboardData> ldData = leaderboardDataService.getAllData(); 
         for(LeaderboardData data: ldData)
         {
